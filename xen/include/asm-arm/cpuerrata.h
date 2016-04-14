b8327ffd100b912177feeed0561ce5d72fe9f159
Julien Grall <julien.grall@arm.com>
2016-04-14 14:45:07 +0100

    xen/arm: Detect silicon revision and set cap bits accordingly
    After each CPU has been started, we iterate through a list of CPU
    errata to detect CPUs which need from hypervisor code patches.
    
    For each bug there is a function which check if that a particular CPU is
    affected. This needs to be done on every CPUs to cover heterogenous
    system properly.
    
    If a certain erratum has been detected, the capability bit will be set.
    In the case the erratum requires code patching, this will be triggered
    by the call to apply_alternatives.
    
    The code is based on the file arch/arm64/kernel/cpu_errata.c in Linux
    v4.6-rc3.
    
    Signed-off-by: Julien Grall <julien.grall@arm.com>
    Acked-by: Stefano Stabellini <sstabellini@kernel.org>
    
    ---
        Changes in v4:
            - Add missing emacs magic blocks
            - Add Stefano's acked-by
    
        Changes in v3:
            - Move update_cpu_capabilities in a separate patch
            - Update the commit message to mention that workaround may
            not require code patching.
    
        Changes in v2:
            - Use XENLOG_INFO for the loglevel of the message

---
+++ xen/include/asm-arm/cpuerrata.h
@@ -0,0 +1,14 @@
+#ifndef __ARM_CPUERRATA_H
+#define __ARM_CPUERRATA_H
+
+void check_local_cpu_errata(void);
+
+#endif /* __ARM_CPUERRATA_H */
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
