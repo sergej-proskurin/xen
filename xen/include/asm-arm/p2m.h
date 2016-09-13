5e7ab49f137c3403dd02d3fc0355f7b79abf01d4
Sergej Proskurin <proskurin@sec.in.tum.de>
2016-09-13 14:28:20 +0200

    arm/p2m: Add altp2m table flushing routine
    The current implementation differentiates between flushing and
    destroying altp2m views. This commit adds the function altp2m_flush,
    which allows to release all of the alternate p2m views. To make sure
    that we flush alternate p2m's only if they are not used by any vCPU, we
    introduce a counter that tracks the vCPUs that are currently using the
    particular p2m.
    
    Signed-off-by: Sergej Proskurin <proskurin@sec.in.tum.de>
    ---
    Cc: Stefano Stabellini <sstabellini@kernel.org>
    Cc: Julien Grall <julien.grall@arm.com>
    ---
    v2: Pages in p2m->pages are not cleared in p2m_flush_table anymore.
        VMID is freed in p2m_free_one.
        Cosmetic fixes.
    
    v3: Changed the locking mechanism to "p2m_write_lock" inside the
        function "altp2m_flush".
    
        Do not flush but rather teardown the altp2m in the function
        "altp2m_flush".
    
        Exchanged the check "altp2m_vttbr[idx] == INVALID_VTTBR" for
        "altp2m_p2m[idx] == NULL" in "altp2m_flush".
    
    v4: Removed the p2m locking instructions in "altp2m_flush", as they are
        not needed at this point. At this point, altp2m should be inactive and
        thus the particular p2m should not be used by a vCPU. Therefore, we
        added an ASSERT statement to ensure this.
    
        We introduce the counter active_vcpus as part of this patch.

--- xen/include/asm-arm/p2m.h
+++ xen/include/asm-arm/p2m.h
@@ -10,6 +10,8 @@
 #include <xen/p2m-common.h>
 #include <public/memory.h>
 
+#include <asm/atomic.h>
+
 #define paddr_bits PADDR_BITS
 
 #define p2m_switch_vttbr_and_get_flags(ovttbr, nvttbr, flags)       \
@@ -125,6 +127,9 @@ struct p2m_domain {
 
     /* Keeping track on which CPU this p2m was used and for which vCPU */
     uint8_t last_vcpu_ran[NR_CPUS];
+
+    /* Alternate p2m: count of vcpu's currently using this p2m. */
+    atomic_t active_vcpus;
 
     /* Choose between: host/alternate. */
     p2m_class_t p2m_class;
