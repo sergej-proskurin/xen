576b1e85c92cd4abc393584f61d7b6f33fb21b40
Sergej Proskurin <proskurin@sec.in.tum.de>
2017-05-25 19:16:04 +0200

    arm/mem_access: Add short-descriptor based gpt
    This commit adds functionality to walk the guest's page tables using the
    short-descriptor translation table format for both ARMv7 and ARMv8. The
    implementation is based on ARM DDI 0487B-a J1-6002 and ARM DDI 0406C-b
    B3-1506.
    
    Signed-off-by: Sergej Proskurin <proskurin@sec.in.tum.de>
    ---
    Cc: Stefano Stabellini <sstabellini@kernel.org>
    Cc: Julien Grall <julien.grall@arm.com>
    ---
    v3: Move the implementation to ./xen/arch/arm/guest_copy.c.
    
        Use defines instead of hardcoded values.
    
        Cosmetic fixes & Added more coments.

--- xen/include/asm-arm/guest_walk.h
+++ xen/include/asm-arm/guest_walk.h
@@ -10,6 +10,22 @@
 #define TTBR0_VALID                         (0)
 #define TTBR1_VALID                         (1)
 
+/* First level translation table descriptor types used by the AArch32
+ * short-descriptor translation table format. */
+#define L1DESC_INVALID                      (0)
+#define L1DESC_PAGE_TABLE                   (1)
+#define L1DESC_SECTION                      (2)
+#define L1DESC_SECTION_PXN                  (3)
+
+/* Defines for section and supersection shifts. */
+#define L1DESC_SECTION_SHIFT                (20)
+#define L1DESC_SUPERSECTION_SHIFT           (24)
+#define L1DESC_SUPERSECTION_EXT_BASE1_SHIFT (32)
+#define L1DESC_SUPERSECTION_EXT_BASE2_SHIFT (36)
+
+/* Second level translation table descriptor types. */
+#define L2DESC_INVALID                      (0)
+
 /* Walk the guest's page tables in software. */
 int guest_walk_tables(const struct vcpu *v,
                       vaddr_t gva,
